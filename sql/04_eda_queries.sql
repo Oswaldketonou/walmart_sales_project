42babbb71e05c46b6908ecb03185ac1ecfcd3495
Oswaldketonou <ketonou@hotmail.com>
2026-03-01 22:25:56 -0600

    Update 04_eda_queries.sql

--- sql/04_eda_queries.sql
+++ sql/04_eda_queries.sql
@@ -2,13 +2,13 @@
 -- Walmart Sales Forecasting Project
 -- 04_eda.sql
 -- Author: Waldo Ketonou | WaldoSphere Group LLC
--- Purpose: Exploratory Data Analysis on walmart_master_fe_final
+-- Purpose: Exploratory Data Analysis on walmart_master_fe
 -- =====================================================================
 
 
 -- 1. BASIC ROW COUNT
 SELECT COUNT(*) AS total_rows
-FROM walmart_master_fe_final;
+FROM walmart_master_fe;
 
 
 
@@ -19,7 +19,7 @@ SELECT
     AVG(weekly_sales) AS avg_sales,
     PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY weekly_sales) AS median_sales,
     STDDEV(weekly_sales) AS stddev_sales
-FROM walmart_master_fe_final;
+FROM walmart_master_fe;
 
 
 
@@ -30,7 +30,7 @@ SELECT
     AVG(weekly_sales) AS avg_sales,
     MIN(weekly_sales) AS min_sales,
     MAX(weekly_sales) AS max_sales
-FROM walmart_master_fe_final
+FROM walmart_master_fe
 GROUP BY store_id
 ORDER BY store_id;
 
@@ -43,7 +43,7 @@ SELECT
     AVG(weekly_sales) AS avg_sales,
     MIN(weekly_sales) AS min_sales,
     MAX(weekly_sales) AS max_sales
-FROM walmart_master_fe_final
+FROM walmart_master_fe
 GROUP BY dept_id
 ORDER BY dept_id;
 
@@ -67,7 +67,7 @@ SELECT
     year,
     month,
     AVG(weekly_sales) AS avg_monthly_sales
-FROM walmart_master_fe_final
+FROM walmart_master_fe
 GROUP BY year, month
 ORDER BY year, month;
 
@@ -78,7 +78,7 @@ SELECT
     year,
     week,
     AVG(weekly_sales) AS avg_weekly_sales
-FROM walmart_master_fe_final
+FROM walmart_master_fe
 GROUP BY year, week
 ORDER BY year, week;
 
@@ -89,7 +89,7 @@ SELECT
     store_id,
     dept_id,
     AVG(weekly_sales) AS avg_sales
-FROM walmart_master_fe_final
+FROM walmart_master_fe
 GROUP BY store_id, dept_id
 ORDER BY store_id, dept_id;
 
@@ -104,7 +104,7 @@ SELECT
     CORR(weekly_sales, lag_3) AS corr_sales_lag3,
     CORR(weekly_sales, roll_avg_3) AS corr_sales_roll3,
     CORR(weekly_sales, roll_avg_5) AS corr_sales_roll5
-FROM walmart_master_fe_final;
+FROM walmart_master_fe;
 
 
 
@@ -113,7 +113,7 @@ SELECT
     year,
     SUM(weekly_sales) AS total_sales,
     AVG(weekly_sales) AS avg_sales
-FROM walmart_master_fe_final
+FROM walmart_master_fe
 GROUP BY year
 ORDER BY year;
 
