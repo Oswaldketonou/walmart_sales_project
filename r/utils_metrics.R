62b1942d1329a1d0faba9df71861206dad9bfcde
Oswaldketonou <ketonou@hotmail.com>
2026-03-03 19:03:02 -0600

    Update utils_metrics.R

--- r/utils_metrics.R
+++ r/utils_metrics.R
@@ -1,1 +1,27 @@
+# ===============================
+# utils_metrics.R
+# Custom Error Metric Functions
+# ===============================
+
+# Root Mean Squared Error
+rmse <- function(actual, predicted) {
+  sqrt(mean((actual - predicted)^2, na.rm = TRUE))
+}
+
+# Mean Absolute Error
+mae <- function(actual, predicted) {
+  mean(abs(actual - predicted), na.rm = TRUE)
+}
+
+# Mean Absolute Percentage Error
+mape <- function(actual, predicted) {
+  mean(abs((actual - predicted) / actual), na.rm = TRUE) * 100
+}
+
+# Symmetric Mean Absolute Percentage Error
+smape <- function(actual, predicted) {
+  numerator <- abs(actual - predicted)
+  denominator <- (abs(actual) + abs(predicted)) / 2
+  mean(numerator / denominator, na.rm = TRUE) * 100
+}
 
